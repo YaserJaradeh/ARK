3d1aba1e8eb2584e21de53bc835d047c4ded6894
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2021-06-06 14:15:31 +0200

    Added streamlit_app.py following https://github.com/streamlit/component-template/blob/master/examples/RadioButton/radio_button.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import os
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+
+_terminal = components.declare_component(
+    "terminal", path=build_dir
+)
