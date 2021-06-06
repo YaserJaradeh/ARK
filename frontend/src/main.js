f4deccc0fc8ccf90d9a7d3deb8ed6e8dd5e61a56
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2021-06-06 17:03:53 +0200

    Add ready signal to main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import shell from "vue-shell";
+import { Streamlit } from "streamlit-component-lib";
 
 Vue.config.productionTip = false;
 Vue.use(shell);
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+Streamlit.setComponentReady();
+
+Streamlit.setFrameHeight(600);
